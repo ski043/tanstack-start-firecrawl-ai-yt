69ec1312464d29ed4d18c118d8df647e7c1de659
Jan Marshal <jan@marshalcode.com>
2025-12-30 16:02:58 +0100

    asdf

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from 'better-auth/react'
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: 'https://test-stuff-123.netlify.app',
+  baseURL: 'https://tanstack-start-firecrawl-ai-yt.vercel.app',
 })
