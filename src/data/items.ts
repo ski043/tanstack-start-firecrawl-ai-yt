0bb502b02b5b3c3d023f1722875b910edb312966
Jan Marshal <jan@marshalcode.com>
2025-12-30 15:40:02 +0100

    feat: Update `better-auth` and `zod` dependencies, and enable prompt for data extraction in `items.ts`.

--- src/data/items.ts
+++ src/data/items.ts
@@ -32,8 +32,8 @@ export const scrapeUrlFn = createServerFn({ method: 'POST' })
           'markdown',
           {
             type: 'json',
-            schema: extractSchema,
-            //prompt: 'please extract the author and also publishedAt timestamp',
+            //schema: extractSchema,
+            prompt: 'please extract the author and also publishedAt timestamp',
           },
         ],
         location: { country: 'US', languages: ['en'] },
@@ -132,8 +132,9 @@ export const bulkScrapeUrlsFn = createServerFn({ method: 'POST' })
             'markdown',
             {
               type: 'json',
-              schema: extractSchema,
-              //prompt: 'please extract the author and also publishedAt timestamp',
+              //
+              prompt:
+                'please extract the author and also publishedAt timestamp',
             },
           ],
           location: { country: 'US', languages: ['en'] },
